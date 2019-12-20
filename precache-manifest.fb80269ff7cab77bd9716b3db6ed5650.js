cdfa05684bd27ac20d8da3901165c360fbcb9800
Carolyn <carolynwk@gmail.com>
2019-12-19 20:16:42 -0800

    Updates

---
+++ precache-manifest.fb80269ff7cab77bd9716b3db6ed5650.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "983db7b3688a9f42c7a9e2c23ef03fa8",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "1e06c2670706614e2bac",
+    "url": "/clicky-game/static/css/main.ef3e2a8f.chunk.css"
+  },
+  {
+    "revision": "b8e8739702a3d076ee0c",
+    "url": "/clicky-game/static/js/2.51c18a40.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/clicky-game/static/js/2.51c18a40.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1e06c2670706614e2bac",
+    "url": "/clicky-game/static/js/main.aa826228.chunk.js"
+  },
+  {
+    "revision": "76da5bcc0e2cfb61c97a",
+    "url": "/clicky-game/static/js/runtime-main.39dcd64a.js"
+  }
+]);
